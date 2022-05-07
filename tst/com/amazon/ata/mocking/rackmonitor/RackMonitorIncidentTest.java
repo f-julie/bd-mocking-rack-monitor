985ea15e3b1899e3fe69e11c02b6a7bb3c6adaf1
FrankJFella <89809152+FrankJFella@users.noreply.github.com>
2022-05-07 12:22:15 -0400

    Update RackMonitorIncidentTest.java

--- tst/com/amazon/ata/mocking/rackmonitor/RackMonitorIncidentTest.java
+++ tst/com/amazon/ata/mocking/rackmonitor/RackMonitorIncidentTest.java
@@ -16,6 +16,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.MockitoAnnotations.initMocks;  // initMocks is deprecated, hence the strikeout
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
